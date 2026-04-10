44fe046ba64b4767cd6ad75c9c69dc21b685a618
gzx <xiaoguo4072@163.com>
2026-04-10 15:54:07 +0800

    fix:uniapp初始化

--- my-first-next/app/admin/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { AdminShell } from "@/components/admin/layout/AdminShell";
-
-export default function AdminLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <AdminShell>{children}</AdminShell>;
-}
--- my-first-next/app/posts/new/page.tsx
+++
@@ -1,5 +0,0 @@
-import { WritePostEditor } from "@/components/post/editor/WritePostEditor";
-
-export default function NewPostPage() {
-  return <WritePostEditor />;
-}
